99c708e97b15d844828f88cf48fbd9f93a178b7e
Lara Souza <115436942+BlackStorm429@users.noreply.github.com>
2023-03-02 12:02:19 -0300

    Update Main.java

--- Exercicio02/src/main/java/Main.java
+++ Exercicio02/src/main/java/Main.java
@@ -1,44 +1,54 @@
-public class Principal {
-
-    public static void main(String[] args) 
+public class Main 
+{
+    public static void main (String [] args) 
     {
-        Carro carro = new Carro();  
-        DAO dao = new DAO();
-        dao.conectar();
+        Moto moto = new Moto ();  
+        DAO dao = new DAO ();
+        dao.conectar ();
         int opcao;
-        System.out.println("MENU:\n");
-        System.out.println("OPÇÕES:\n");
-        System.out.println(
+        System.out.println ("MENU:\n");
+        System.out.println ("OPÇÕES:\n");
+        System.out.println (
                 "1-listar\n" + "2-Inserir\n" + "3-Excluir\n" + "4-Atualizar\n" + "5-Sair\n");
-        opcao = MyIO.readInt();
-        switch (opcao) {
+        opcao = MyIO.readInt ();
+        
+        switch (opcao) 
+        {
             case 1:
                 // Mostrar usuários
-                Carro[] carros = dao.getCarros();
-                System.out.println("==== Mostrar usuários === ");
-                for (int i = 0; i < carros.length; i++) {
-                    System.out.println(carros[i].toString());
+                Moto [] motos = dao.getMotos ();
+                System.out.println ("==== Mostrar usuários === ");
+                
+                for (int i = 0; i < motos.length; i++) 
+                {
+                    System.out.println (motos [i].toString ());
                 }
+                
             case 2:
                 // Inserir um elemento na tabela
-                if (dao.inserirCarro(carro) == true) {
-                    System.out.println("Inserção com sucesso -> " + carro.toString());
+                if (dao.inserirMoto (moto) == true) 
+                {
+                    System.out.println ("Inserção com sucesso -> " + moto.toString());
                 }
+                
             case 3:
                 // Excluir usuário
-                dao.excluirCarro(carro.getId());
+                dao.excluirCarro (moto.getId ());
                 break;
+                
             case 4:
                 // Atualizar usuário
-                carro.setModelo("picape");
-                dao.atualizarCarro(carro);
+                carro.setModelo ("scooter");
+                dao.atualizarCarro (moto);
+                
             case 5:
-                dao.close();
+                dao.close ();
                 break;
 
             default:
                 break;
         }
-        dao.close();
+        
+        dao.close ();
     }
 }
