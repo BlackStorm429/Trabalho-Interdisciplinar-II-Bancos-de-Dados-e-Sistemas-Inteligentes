35d3f78a1f157600ece44a896866f40febe2c700
Lara Souza <115436942+BlackStorm429@users.noreply.github.com>
2023-03-02 11:49:23 -0300

    Update Carro.java

--- Exercicio02/src/main/java/Carro.java
+++ Exercicio02/src/main/java/Carro.java
@@ -3,61 +3,54 @@ public class Carro
     private int id;
     private String modelo;
     private int ano;
-    
 
-    public Carro()
+    public Carro ()
     {
         id = -1;
         modelo = "";
         ano = -1;
     }
-    public Carro(int id, String modelo, int ano) 
+	
+    public Carro (int id, String modelo, int ano) 
     {
         this.id = id;
         this.modelo = modelo;
         this.ano = ano;
     }
 
-
-    public int getId() 
+    public int getId () 
     {
         return id;
     }
 
-
-    public void setId(int id) 
+    public void setId (int id) 
     {
         this.id = id;
     }
 
-
-    public String getModelo() 
+    public String getModelo () 
     {
         return modelo;
     }
 
-
-    public void setModelo(String modelo) 
+    public void setModelo (String modelo) 
     {
         this.modelo = modelo;
     }
 
-
-    public int getAno() 
+    public int getAno () 
     {
         return ano;
     }
 
-
-    public void setAno(int ano) {
-
+    public void setAno (int ano) 
+    {
         this.ano = ano;
     }
 
     @Override
-	public String toString() 
+    public String toString () 
     {
-		return "Usuario [id=" + id + ",modelo=" +modelo + ", ano=" + ano + "]";
-	}
-    
+	return "Usuario [id=" + id + ",modelo=" +modelo + ", ano=" + ano + "]";
+    }  
 }
