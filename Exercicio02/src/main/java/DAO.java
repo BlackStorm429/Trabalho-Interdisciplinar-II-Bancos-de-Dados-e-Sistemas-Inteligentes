e8b50cbf7d54011932833de26dd52f6314964116
Lara Souza <115436942+BlackStorm429@users.noreply.github.com>
2023-03-02 11:58:01 -0300

    Update DAO.java

--- Exercicio02/src/main/java/DAO.java
+++ Exercicio02/src/main/java/DAO.java
@@ -2,115 +2,158 @@
 
 import java.sql.*;
 
-public class DAO {
+public class DAO 
+{
 	private Connection conexao;
 	
-	public DAO() {
+	public DAO() 
+	{
 		conexao = null;
 	}
 	
-	public boolean conectar() {
+	public boolean conectar () 
+	{
 		String driverName = "org.postgresql.Driver";                    
 		String serverName = "localhost";
-		String mydatabase = "Carro";
+		String mydatabase = "Moto";
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "ti2cc";
 		String password = "ti@cc";
 		boolean status = false;
 
-		try {
-			Class.forName(driverName);
-			conexao = DriverManager.getConnection(url, username, password);
+		try 
+		{
+			Class.forName (driverName);
+			conexao = DriverManager.getConnection (url, username, password);
 			status = (conexao == null);
-			System.out.println("Conexão efetuada com o postgres!");
-		} catch (ClassNotFoundException e) { 
-			System.err.println("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage());
-		} catch (SQLException e) {
-			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
+			System.out.println ("Conexão efetuada com o postgres!");
+		} 
+		
+		catch (ClassNotFoundException e) 
+		{ 
+			System.err.println ("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage ());
+		} 
+		
+		catch (SQLException e) 
+		{
+			System.err.println ("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
 		}
 
 		return status;
 	}
 	
-	public boolean close() {
+	public boolean close () 
+	{
 		boolean status = false;
 		
-		try {
-			conexao.close();
+		try 
+		{
+			conexao.close ();
 			status = true;
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
+		} 
+		
+		catch (SQLException e) 
+		{
+			System.err.println (e.getMessage ());
 		}
+		
 		return status;
 	}
 	
-	public boolean inserirCarro(Carro carro) {
+	public boolean inserirMoto (Moto moto) 
+	{
 		boolean status = false;
-		try {  
-			Statement st = conexao.createStatement();
-			st.executeUpdate("INSERT INTO carro (id, modelo, ano) "
-					       + "VALUES ("+carro.getId()+ ", '" + carro.getModelo() + "', '"  
-					       + carro.getAno());
-			st.close();
+		try 
+		{  
+			Statement st = conexao.createStatement ();
+			st.executeUpdate ("INSERT INTO moto (id, modelo, ano) "
+					       + "VALUES ("+moto.getId ()+ ", '" + moto.getModelo () + "', '"  
+					       + moto.getAno ());
+			st.close ();
 			status = true;
-		} catch (SQLException u) {  
-			throw new RuntimeException(u);
+		} 
+		
+		catch (SQLException u) 
+		{  
+			throw new RuntimeException (u);
 		}
+		
 		return status;
 	}
 	
-	public boolean atualizarCarro(Carro carro) {
+	public boolean atualizarMoto (Moto moto) 
+	{
 		boolean status = false;
-		try {  
-			Statement st = conexao.createStatement();
-			String sql = "UPDATE carro SET modelo = '" + carro.getModelo() + "', senha = '"  
-				       + carro.getAno()+ "'"
-					   + " WHERE id = " + carro.getId();
+		
+		try 
+		{  
+			Statement st = conexao.createStatement ();
+			String sql = "UPDATE moto SET modelo = '" + moto.getModelo () + "', senha = '"  
+				       + moto.getAno()+ "'"
+					   + " WHERE id = " + moto.getId();
 			st.executeUpdate(sql);
 			st.close();
 			status = true;
-		} catch (SQLException u) {  
-			throw new RuntimeException(u);
+		} 
+		
+		catch (SQLException u) 
+		{  
+			throw new RuntimeException (u);
 		}
+		
 		return status;
 	}
 	
-	public boolean excluirCarro(int id) {
+	public boolean excluirMoto (int id) 
+	{
 		boolean status = false;
-		try {  
-			Statement st = conexao.createStatement();
-			st.executeUpdate("DELETE FROM carro WHERE id = " + id);
-			st.close();
+		
+		try 
+		{  
+			Statement st = conexao.createStatement ();
+			st.executeUpdate ("DELETE FROM carro WHERE id = " + id);
+			st.close ();
 			status = true;
-		} catch (SQLException u) {  
+		} 
+		
+		catch (SQLException u) 
+		{  
 			throw new RuntimeException(u);
 		}
+		
 		return status;
 	}
 	
 	
-	public Carro[] getCarros() 
+	public Moto [] getMotos () 
     {
-		Carro[] Carros = null;
+		Moto [] Motos = null;
 		
 		try {
-			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-			ResultSet rs = st.executeQuery("SELECT * FROM Carro");		
-	         if(rs.next()){
-	             rs.last();
-	             Carros = new Carro[rs.getRow()];
-	             rs.beforeFirst();
+			Statement st = conexao.createStatement (ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = st.executeQuery ("SELECT * FROM Moto");	
+			
+			if (rs.next())
+			{
+				rs.last();
+				Carros = new Moto [rs.getRow ()];
+				rs.beforeFirst ();
 
-	             for(int i = 0; rs.next(); i++) {
-	                Carros[i] = new Carro(rs.getInt("id"), rs.getString("modelo"), 
-	                		                  rs.getInt("ano"));
-	             }
-	          }
-	          st.close();
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
+				for (int i = 0; rs.next(); i++) 
+				{
+					Carros [i] = new Moto (rs.getInt ("id"), rs.getString ("modelo"), 
+									  rs.getInt ("ano"));
+				}
+	            }
+	          st.close ();
+		} 
+		
+		catch (Exception e) 
+		{
+			System.err.println (e.getMessage ());
 		}
-		return Carros;
+		
+		return Motos;
 	}
 }
